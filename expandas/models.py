c18dc40637a708d9171bec8e5ff81c0b65c53261
Eric Lindsjö <eric.lindsjo@glesys.se>
2016-09-09 20:56:23 +0200

    Added data model classes

---
+++ expandas/models.py
@@ -0,0 +1,27 @@
+class ASSet:
+    def __init__(self, name, **kwargs):
+    
+        if not "members" in kwargs:
+            raise Exception("Please supply a list of members")
+
+        self.name = name
+        self.members = kwargs["members"]
+        self.inet = []
+        self.inet6 = []
+
+        # Copy member prefixes to our own prefix list
+        for member in self.members:
+            self.inet += member.inet
+            self.inet6 += member.inet6
+
+class ASNumber:
+    def __init__(self, asn, **kwargs):
+
+    if not "inet" in kwargs:
+        raise("Please supply a list of inet prefixes")
+    if not "inet6" in kwargs:
+        raise("Please supply a list of inet6 prefixes")
+
+    self.asn = asn
+    self.inet = kwargs["inet"]
+    self.inet6 = kwargs["inet6"]
