a6bf19f1a6d3c26ea05f4bed75bca8afe7bbec83
Eric Lindsjö <eric.lindsjo@glesys.se>
2016-09-11 15:15:49 +0200

    Adds methods to check if models contain network or asn

--- expandas/model.py
+++ expandas/model.py
@@ -1,4 +1,23 @@
-class ASSet:
+class BaseModel:
+    """ Abstract base model, implements methods used by both ASSet and ASNumber """
+
+    def contains(self, inet):
+        """ Check if as-set or asnumber contains a specified network """
+        import ipaddress
+        ip = ipaddress.ip_network(inet)
+
+        if ip.version == 4:
+            for i in self.inet:
+                if i.overlaps(ip):
+                    return True
+            return False
+        elif ip.version == 6:
+            for i in self.inet6:
+                if i.overlaps(ip):
+                    return True
+            return False
+
+class ASSet(BaseModel):
     def __init__(self, name, **kwargs):
     
         if not "members" in kwargs:
@@ -14,7 +33,12 @@ class ASSet:
             self.inet += member.inet
             self.inet6 += member.inet6
 
-class ASNumber:
+    def contains_asn(self, asn):
+        if [ a for a in self.members if a.asn == int(asn) ]:
+            return True
+        return False
+
+class ASNumber(BaseModel):
     def __init__(self, asn, **kwargs):
 
         if not "inet" in kwargs:
@@ -22,6 +46,6 @@ class ASNumber:
         if not "inet6" in kwargs:
             raise("Please supply a list of inet6 prefixes")
 
-        self.asn = asn
+        self.asn = int(asn)
         self.inet = kwargs["inet"]
         self.inet6 = kwargs["inet6"]
